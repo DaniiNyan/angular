5c13d19ff97d0472a9981d097764def448d9718d
daniinyan <daniela.dbass@gmail.com>
2019-09-25 16:20:32 -0300

    Add color backgroung to server status

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   serverId: number = 52;
   serverStatus: string = 'running';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'running' : 'disconnected';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'running' ? 'dodgerblue' : 'pink';
   }
 }
