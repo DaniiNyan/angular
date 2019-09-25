a217205dd3fd5e05f42d1be4d77513e08be32c58
daniinyan <daniela.dbass@gmail.com>
2019-09-25 16:27:55 -0300

    Aplly dynamic CSS with ngClass

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
   serverId: number = 52;
@@ -18,5 +19,9 @@ export class ServerComponent {
 
   getColor() {
     return this.serverStatus === 'running' ? 'dodgerblue' : 'pink';
+  }
+
+  isRunning() {
+    return this.serverStatus === 'running'; 
   }
 }
